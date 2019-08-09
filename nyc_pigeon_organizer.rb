58a97df166fbd4f8b105e009590203b965985797
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 22:16:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+
+  data.each do |descriptors, descriptions|
+    descriptions.each do |description, names|
+      names.each do |name|
+        new_data[name] ||= {}
+        new_data[name][descriptors] ||= []
+        new_data[name][descriptors].push description.to_s
+      end
+    end
+  end
+  new_data
 end
