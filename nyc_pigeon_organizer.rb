5ffaf736d785964b7f125db435c6340241c23689
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 22:15:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each do |descriptors, descriptions|
     descriptions.each do |description, names|
       names.each do |name|
-        new_data[name] ||= {}
-        new_data[name][descriptors] ||= []
+        new_data[name] || new_data[name] = {}
+        new_data[name][descriptors] || new_data[name][descriptors] = []
         new_data[name][descriptors] << description.to_s
       end
     end
